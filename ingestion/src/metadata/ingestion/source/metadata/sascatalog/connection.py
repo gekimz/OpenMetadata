f834836cca4ed6dee9e10272c47a91c8bb5bf8c9
root <root@SAS-R90S3VBF>
2023-06-15 13:26:15 -0400

    Created source connection handler for SAS Catalog

---
+++ ingestion/src/metadata/ingestion/source/metadata/sascatalog/connection.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+from metadata.generated.schema.entity.entity.automations.workflow import (
+    Workflow as AutomationWorkflow,
+)
+from metadata.generated.schema.entity.services.connections.metadata.sasCatalogConnection import (
+    SASCatalogConnection,
+)
+from metadata.ingestion.connections.test_connections import test_connection_steps
+from metadata.ingestion.ometa.ometa_api import OpenMetadata
+from metadata.ingestion.source.metadata.sascatalog.client import SASCatalogClient
+
+
+def get_connection(connection: SASCatalogConnection) -> SASCatalogClient:
+    return SASCatalogClient(connection)
+
+
+def test_connection(
+    metadata: OpenMetadata,
+    client: SASCatalogClient,
+    service_connection: SASCatalogConnection,
+    automation_workflow: Optional[AutomationWorkflow] = None,
+) -> None:
+    test_fn = {"CheckAccess": client.list_instances}
+    test_connection_steps(
+        metadata=metadata,
+        test_fn=test_fn,
+        service_type=service_connection.type.value,
+        automation_workflow=automation_workflow,
+    )
